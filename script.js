d59df8cca13ee9c555e70d15edc2179802199598
Christopher Harris <christopher.harris@moneysupermarket.com>
2023-01-05 17:40:18 +0000

    JS: Further JS Development

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log("Hello World!");
+//console.log("Hello World!");
 
 const btn = document.querySelector(".btn-open");
 const form = document.querySelector(".fact-form");
@@ -13,29 +13,62 @@ btn.addEventListener("click", function () {
   }
 });
 
-let votesInteresting = 23;
-let votesMindblowing = 5;
-const text = "London is the capital of England";
-
-votesInteresting = votesInteresting + 1;
-votesInteresting++;
-console.log(votesInteresting);
-
-let totalUpvotes = votesInteresting + votesMindblowing;
-console.log("Upvotes:", totalUpvotes);
-
-let votesFalse = 4;
-const isCorrect = votesFalse < totalUpvotes;
-console.log(isCorrect);
-
 function calcFactAge(year) {
   const currentYear = new Date().getFullYear();
   const age = currentYear - year;
-  return age;
+
+  if (age >= 0) return age;
+  else return "Impossible Year";
 }
 
 const age1 = calcFactAge(2016);
 console.log(age1);
-
 console.log(calcFactAge(2020));
 console.log(calcFactAge(1990));
+console.log(calcFactAge(2037));
+
+let votesInteresting = 20;
+let votesMindblowing = 25;
+
+if (votesInteresting === votesMindblowing) {
+  alert("This fact is equally interesting and mind blowing!");
+} else if (votesInteresting > votesMindblowing) {
+  console.log("Interesting fact!");
+} else if (votesMindblowing > votesInteresting) {
+  console.log("Minblowing fact!!!");
+}
+
+//falsy values: 0, '', null, undefined
+let falsyvalue = 0;
+//truthy values: everything else..
+if (falsyvalue) {
+  console.log("Truthy value");
+} else {
+  console.log("Falsy value");
+}
+
+let votesFalse = 77;
+const totalUpvotes = votesInteresting + votesMindblowing;
+
+const message =
+  totalUpvotes > votesFalse
+    ? "The fact is true"
+    : "The fact may be false, check more sources..";
+
+alert(message);
+
+// ###### Practice: #######
+// let votesInteresting = 23;
+// let votesMindblowing = 5;
+// const text = "London is the capital of England";
+
+// votesInteresting = votesInteresting + 1;
+// votesInteresting++;
+// console.log(votesInteresting);
+
+// let totalUpvotes = votesInteresting + votesMindblowing;
+// console.log("Upvotes:", totalUpvotes);
+
+// let votesFalse = 4;
+// const isCorrect = votesFalse < totalUpvotes;
+// console.log(isCorrect);
