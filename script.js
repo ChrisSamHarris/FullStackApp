b20363e02d5ddedb368bb5049f46fafbce994be0
Christopher Harris <christopher.harris@moneysupermarket.com>
2023-01-06 11:05:51 +0000

    JS: Further JS Development

--- script.js
+++ script.js
@@ -22,17 +22,17 @@ function calcFactAge(year) {
 }
 
 const age1 = calcFactAge(2016);
-console.log(age1);
-console.log(calcFactAge(2020));
-console.log(calcFactAge(1990));
+// console.log(age1);
+// console.log(calcFactAge(2020));
+// console.log(calcFactAge(1990));
 //console.log(calcFactAge(2037));
 
 // ####### Arrow Functions #######
 
 const calcFactAge2 = (year) => new Date().getFullYear() - year;
 
-console.log(calcFactAge2(1990));
-console.log(calcFactAge2(2037));
+// console.log(calcFactAge2(1990));
+// console.log(calcFactAge2(2037));
 
 // Advanced arrow function
 const calcFactAge3 = (year) =>
@@ -40,8 +40,8 @@ const calcFactAge3 = (year) =>
     ? new Date().getFullYear() - year
     : `Year needs to be less or equal to ${new Date().getFullYear()}.`;
 
-console.log(calcFactAge3(1990));
-console.log(calcFactAge3(2037));
+// console.log(calcFactAge3(1990));
+// console.log(calcFactAge3(2037));
 
 // let votesInteresting = 20;
 // let votesMindblowing = 25;
@@ -85,17 +85,17 @@ console.log(calcFactAge3(2037));
 
 //  Arrays
 const factArr = ["London is the capital of England", 2015, true];
-console.log(factArr);
-console.log(factArr[2]);
-console.log(factArr.length);
-console.log(factArr[factArr.length - 2]);
+// console.log(factArr);
+// console.log(factArr[2]);
+// console.log(factArr.length);
+// console.log(factArr[factArr.length - 2]);
 
 //const [text, createdIn, isCorrect] = factArr;
 // console.log(text);
 // console.log(createdIn);
 
 const newFactArr = [...factArr, "Society"];
-console.log(newFactArr);
+// console.log(newFactArr);
 
 // Object
 const factObj = {
@@ -109,12 +109,76 @@ const factObj = {
     } is from the category: ${this.category.toUpperCase()}`;
   },
 };
-console.log(factObj.text);
-console.log(factObj["text"]);
-console.log(factObj.createSummary());
+// console.log(factObj.text);
+// console.log(factObj["text"]);
+// console.log(factObj.createSummary());
 
 const { category, isCorrect } = factObj;
 //console.log(category);
+
+[2, 4, 6, 8].forEach(function (el) {
+  //console.log(el);
+});
+
+const times10 = [2, 4, 6, 8].map(function (el) {
+  return el * 10;
+});
+//console.log(times10);
+
+const times20 = [2, 4, 6, 8].map((el) => el * 20);
+//console.log(times20);
+
+const CATEGORIES = [
+  { name: "technology", color: "#3b82f6" },
+  { name: "science", color: "#16a34a" },
+  { name: "finance", color: "#ef4444" },
+  { name: "society", color: "#eab308" },
+  { name: "entertainment", color: "#db2777" },
+  { name: "health", color: "#14b8a6" },
+  { name: "history", color: "#f97316" },
+  { name: "news", color: "#8b5cf6" },
+];
+
+const allCategories = CATEGORIES.map((el) => el.name);
+console.log(allCategories);
+
+const initialFacts = [
+  {
+    id: 1,
+    text: "React is being developed by Meta (formerly facebook)",
+    source: "https://opensource.fb.com/",
+    category: "technology",
+    votesInteresting: 24,
+    votesMindblowing: 9,
+    votesFalse: 4,
+    createdIn: 2021,
+  },
+  {
+    id: 2,
+    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
+    source:
+      "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
+    category: "society",
+    votesInteresting: 11,
+    votesMindblowing: 2,
+    votesFalse: 0,
+    createdIn: 2019,
+  },
+  {
+    id: 3,
+    text: "Lisbon is the capital of Portugal",
+    source: "https://en.wikipedia.org/wiki/Lisbon",
+    category: "society",
+    votesInteresting: 8,
+    votesMindblowing: 3,
+    votesFalse: 1,
+    createdIn: 2015,
+  },
+];
+
+const factAges = initialFacts.map((el) => calcFactAge3(el.createdIn));
+console.log(factAges);
+console.log(factAges.join(" & "));
 
 // // ###### Practice: #######
 // // let votesInteresting = 23;
