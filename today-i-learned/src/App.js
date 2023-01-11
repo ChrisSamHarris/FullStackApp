cc1254321221f59cc6aa1f572268f02999f284aa
Christopher Harris <christopher.harris@moneysupermarket.com>
2023-01-11 18:23:16 +0000

    React: Transformed to react, pulling data externally from supabase - requirement to interact with the data and upload further data

---
+++ today-i-learned/src/App.js
@@ -0,0 +1,281 @@
+// ################### ----- Metadata ----- ###################
+import { useEffect, useState } from "react";
+import supabase from "./supabase";
+
+import "./style.css";
+
+const CATEGORIES = [
+  { name: "technology", color: "#3b82f6" },
+  { name: "science", color: "#16a34a" },
+  { name: "finance", color: "#ef4444" },
+  { name: "society", color: "#eab308" },
+  { name: "entertainment", color: "#db2777" },
+  { name: "health", color: "#14b8a6" },
+  { name: "history", color: "#f97316" },
+  { name: "news", color: "#8b5cf6" },
+];
+
+const initialFacts = [
+  {
+    id: 1,
+    text: "React is being developed by Meta (formerly facebook)",
+    source: "https://opensource.fb.com/",
+    category: "technology",
+    votesInteresting: 24,
+    votesMindblowing: 9,
+    votesFalse: 4,
+    createdIn: 2021,
+  },
+  {
+    id: 2,
+    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
+    source:
+      "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
+    category: "society",
+    votesInteresting: 11,
+    votesMindblowing: 2,
+    votesFalse: 0,
+    createdIn: 2019,
+  },
+  {
+    id: 3,
+    text: "Lisbon is the capital of Portugal",
+    source: "https://en.wikipedia.org/wiki/Lisbon",
+    category: "society",
+    votesInteresting: 8,
+    votesMindblowing: 3,
+    votesFalse: 1,
+    createdIn: 2015,
+  },
+];
+
+// END ///// ################### ----- Metadata ----- ###################
+
+// Component in react = React Function => can only return one element per function
+function Counter() {
+  const [count, setCount] = useState(0);
+  console.log("rendering...");
+  //btn.addEventListener('click', function()..)
+
+  return (
+    <div>
+      <span style={{ fontSize: "40px" }}>{count}</span>
+      <button
+        className="btn btn-large"
+        onClick={() => setCount((count) => count + 1)}
+      >
+        +1
+      </button>
+      <button
+        className="btn btn-large"
+        onClick={() => setCount((count) => (count = 0))}
+      >
+        reset
+      </button>
+    </div>
+  );
+}
+
+function App() {
+  const [showForm, setShowForm] = useState(false);
+  const [facts, setFacts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function getFacts() {
+      // async returns a promise
+      setIsLoading(true);
+      const { data: facts, error } = await supabase
+        .from("facts")
+        .select("*")
+        .order("votesInteresting", { ascending: false })
+        .limit(100);
+      console.log(error);
+      // real world = pagination? multiple pages etc.
+      setFacts(facts);
+      setIsLoading(false);
+    }
+    getFacts();
+  }, []);
+  // Dependency Array?
+
+  return (
+    <>
+      {/* Objects exported to chil function through declarations of K:V in headers  */}
+      <Header showForm={showForm} setShowForm={setShowForm} />
+
+      {/* <Counter /> */}
+      {/* Use state variable */}
+      {showForm ? (
+        <NewFactForm setFacts={setFacts} setShowForm={setShowForm} />
+      ) : null}
+      <main className="main">
+        <CategoryFilter />
+        {isLoading ? <Loader /> : <FactList facts={facts} />}
+      </main>
+    </>
+  );
+}
+
+function Loader() {
+  return <p className="loadMessage">Loading... </p>;
+}
+
+// Header uses destructured objects from the App function
+function Header({ showForm, setShowForm }) {
+  const appTitle = "Today I learned";
+
+  return (
+    <header className="header">
+      <div className="logo">
+        <img src="./logo.png" alt="Today I Learned Logo" />
+        <h1>{appTitle}</h1>
+      </div>
+
+      <button
+        className="btn btn-large btn-open"
+        // update state variable
+        onClick={() => setShowForm((show) => !show)}
+      >
+        {showForm ? "Close" : "Share a fact"}
+      </button>
+    </header>
+  );
+}
+
+function isValidHttpUrl(string) {
+  let url;
+  try {
+    url = new URL(string);
+  } catch (_) {
+    return false;
+  }
+  return url.protocol === "http:" || url.protocol === "https:";
+}
+
+function NewFactForm({ setFacts, setShowForm }) {
+  const [text, setText] = useState("");
+  const [source, setSource] = useState("");
+  const [category, setCategory] = useState("");
+  const textLength = text.length;
+
+  function handleSubmit(e) {
+    // 1. Prevent browser reload
+    e.preventDefault();
+    console.log(text, source, category);
+    // 2. Check data is valid
+    if (text && isValidHttpUrl(source) && category && textLength <= 200) {
+      // 3. Create a new fact object
+      const newFact = {
+        id: Math.round(Math.random() * 100000),
+        text,
+        source,
+        category,
+        votesInteresting: 0,
+        votesMindblowing: 0,
+        votesFalse: 0,
+        createdIn: new Date().getFullYear(),
+      };
+      // 4.Add a new fact to the UI, add the fact to state
+      setFacts((facts) => [newFact, ...facts]);
+      // 5. Reset input fields
+      setText("");
+      setSource("");
+      setCategory("");
+      // 6. Close the entire form
+      setShowForm(false);
+    }
+  }
+
+  return (
+    <form className="fact-form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Share a fact with the world..."
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <span id="form_limit">{200 - textLength}</span>
+      <input
+        value={source}
+        type="text"
+        placeholder="Trustworthy source..."
+        onChange={(e) => setSource(e.target.value)}
+      />
+      <select value={category} onChange={(e) => setCategory(e.target.value)}>
+        <option value="">Choose Category:</option>
+        {CATEGORIES.map((cat) => (
+          <option key={cat.name} value={cat.name}>
+            {cat.name.charAt(0).toUpperCase() +
+              cat.name.substring(1).toLowerCase()}
+          </option>
+        ))}
+      </select>
+      <button className="btn btn-large">Post</button>
+    </form>
+  );
+}
+
+function CategoryFilter() {
+  return (
+    <aside>
+      <ul>
+        <li>
+          <button className="btn btn-all-categories">All</button>
+        </li>
+        {CATEGORIES.map((cat) => (
+          <li key={cat.name} className="category">
+            <button
+              className="btn btn-category"
+              style={{ backgroundColor: cat.color }}
+            >
+              {cat.name}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </aside>
+  );
+}
+
+function FactList({ facts }) {
+  return (
+    <section>
+      <ul className="facts-list">
+        {facts.map((fact) => (
+          <Fact key={fact.id} fact={fact} />
+        ))}
+      </ul>
+      <p>There are {facts.length} facts in the database. Add your own!</p>
+    </section>
+  );
+}
+
+function Fact({ fact }) {
+  return (
+    <li className="fact">
+      <p>
+        {fact.text}
+        <a className="source" href={fact.source} target="_blank">
+          (Source)
+        </a>
+      </p>
+      <span
+        className="tag"
+        style={{
+          backgroundColor: CATEGORIES.find((cat) => cat.name === fact.category)
+            .color,
+        }}
+      >
+        {fact.category}
+      </span>
+      <div className="vote-buttons">
+        <button>👍 {fact.votesInteresting}</button>
+        <button>🤯 {fact.votesMindblowing}</button>
+        <button>⛔️ {fact.votesFalse}</button>
+      </div>
+    </li>
+  );
+}
+
+export default App;
